3cd3b83803d7819def632dca80074e7eae834e70
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-02-10 16:35:57 +0100

    Update sensor_interface.py

--- sensor_interface.py
+++ sensor_interface.py
@@ -1,7 +1,7 @@
 import random
 
 def read_sensor_data():
-
+ # This simulates sensor reading with noise and occasional stomp indicating toxity
   base = 7
   noise = random.uniform (-0.7, 0.7)
   stomp = random.choice([0, 0, 0, 7]) # Occasional stomp
