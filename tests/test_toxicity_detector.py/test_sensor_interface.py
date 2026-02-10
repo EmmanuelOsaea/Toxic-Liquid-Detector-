199a95b58f1e46fdd326a7bcb05c71eae0073881
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-02-10 20:44:53 +0100

    Update test_sensor_interface.py

--- tests/test_toxicity_detector.py/test_sensor_interface.py
+++ tests/test_toxicity_detector.py/test_sensor_interface.py
@@ -2,4 +2,12 @@ import unittest
 from toxicity_detector import detect_toxicity 
 
 class TestToxicityDetector(unittest.TestCase):
-def
+def test_detect_toxicity(self):
+  rising_data = [7, 8, 9, 10, 11]
+  self.assertTrue(detect_toxicity(rising_data, thresold=10))
+
+  non_rising_data = [7, 8, 7, 9, 8]
+  self.assertFalse(detect_toxicity(rising_data, thresold=10))
+
+if __name__ == "__main__":
+  unittest.main()
