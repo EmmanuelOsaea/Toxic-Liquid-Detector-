3581e92dbb47ebc0f862e999143e0cdde749ec91
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-02-10 16:31:15 +0100

    Update sensor_interface.py

--- sensor_interface.py
+++ sensor_interface.py
@@ -1,1 +1,8 @@
+import random
 
+def read_sensor_data():
+
+  base = 7
+  noise = random.uniform (-0.7, 0.7)
+  stomp = random.choice([0, 0, 0, 7]) # Occasional stomp
+  return base_value+noise+stomp
